58778f268cf65b358d1e8e70ddf1c2bdde7aa4fe
Anderson Moura <andersonmoura109@gmail.com>
2022-07-02 21:00:21 -0300

    Created ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -10,26 +10,74 @@ class Bytebank extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Dashboard'),
+      theme: ThemeData(
+        colorScheme: ColorScheme(
+          brightness: Brightness.light,
+          primary: Colors.green.shade900,
+          onPrimary: Colors.white,
+          secondary: Colors.blueAccent.shade700,
+          onSecondary: Colors.white,
+          error: Colors.red,
+          onError: Colors.white,
+          background: Colors.black,
+          onBackground: Colors.white,
+          surface: Colors.grey,
+          onSurface: Colors.white,
         ),
-        body: Column(
-          children: [
-            Image.asset('assets/images/bytebank_logo.png'),
-            Container(
-              width: 100,
-              height: 120,
-              color: Colors.green,
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent.shade700,
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: const Dashboard(),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('assets/images/bytebank_logo.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              width: 150,
+              height: 100,
+              color: Theme.of(context).colorScheme.primary,
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: const [
-                  Icon(Icons.people),
-                  Text('Contacts'),
+                  Icon(
+                    Icons.people,
+                    size: 24,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    'Contacts',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                    ),
+                  ),
                 ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
